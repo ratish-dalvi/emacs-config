289292049d0e3237f8172ecc6cbec52959c11a92
Ratish <ratish@wise.io>
2016-11-10 14:43:22 -0800

    added my custom config

---
+++ install.sh
@@ -0,0 +1,5 @@
+cp emacs-config to ~/.emacs
+cp -r emacs.d  ~/.emacs.d
+# This could be any directory in path
+dir_pychecker=/home/ratish/wiseio/anaconda/bin/
+cp pychecker.sh $dir_pychecker/pychecker.sh 
